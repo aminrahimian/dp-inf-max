05bb0da203917ffc405305bb2cf77e4b0b5bc542
carloshurtado <CAH259@pitt.edu>
2022-11-16 18:39:16 -0500

    Add comments to the scripts to make it readable

--- Epsilon_differentially.py
+++ Epsilon_differentially.py
@@ -8,24 +8,23 @@ from scipy.stats import bernoulli
 
 # Parameters of the network
 
-n = 1005
-m = 100
-k = 4
-p_ic = 0.03
-s_bulk=40
+n = 1005                    # Number of nodes
+m = 100                     # Number of nodes samples to built matrix X for each influence cascade
+k = 4                       # Number of the seed set
+p_ic = 0.03                 # Probability for Independent Cascade Model
+s_bulk=40                   # Number of Influence cascade model
 # epsilon = 1e-1
 
 
 G_base = nx.read_edgelist("email-Eu-core.txt.gz",nodetype=int, data=(("Type", str),))
 
-def func_test():
 
-    pass
 
-# n is the number of edges not nodes
 def live_edges_saving(n, p, G):
 
-    
+    #    Function to generate Influence cascades.
+    #   Input parameter are n: number of nodes, p: probability of the ICM, the network G.
+
     L=len(G.edges)
    
     tabular=np.zeros((L,1))
@@ -54,6 +53,8 @@ def live_edges_saving(n, p, G):
 
 def save_graph_in_list(m, G_base, p_ic):
     
+    #   This function generates a list where each element is a influence cascade network
+    #   Input as the parameters defined globally.
 
     test_keys = list(G_base.nodes())
     test_values = list(range(len(test_keys)))
@@ -78,13 +79,17 @@ def save_graph_in_list(m, G_base, p_ic):
 
 def load_subgraphs():
 
+    # Function to load pickle files with the list of Influence cascade networks
+
     with open('live_edges.pkl', 'rb') as f:
         m_live_edges = pickle.load(f)
 
     return m_live_edges
 
-
 def built_matrix_X(m, n, m_live_edges):
+
+    # This function generates the matrix X of dimension mxn
+    # Where m and n are the global parameters
 
     matrix_X = np.zeros((m, n))
     indices = list(m_live_edges[0].nodes())
@@ -100,10 +105,10 @@ def built_matrix_X(m, n, m_live_edges):
 
     return matrix_X
 
+def bulk_matrices(s_bulk,m_live_edges,m,n,p_ic):
 
-
-
-def bulk_matrices():
+    #   This function generates and save a set of X matrices.
+    #   The number of matrices are defined by bulk_matrices parameter
 
     bulk_matrices=[]
     
@@ -113,19 +118,14 @@ def bulk_matrices():
         m_live_edges = save_graph_in_list(m, G_float, p_ic)
         bulk_matrices.append(built_matrix_X(m, n, m_live_edges))
 
-    return bulk_matrices
-
-
-
-
-def save_matrix_X():
-
     with open('matrices_X.pkl', 'wb') as f:
         pickle.dump(bulk_matrices, f)
 
+    return bulk_matrices
+
 
 def load_matrix_X():
-
+    # Function to load pickle file with already generate X matrices
     with open('matrices_X.pkl', 'rb') as f:
         matrix_X = pickle.load(f)
 
@@ -134,9 +134,6 @@ def load_matrix_X():
 
 # save_matrix_X()
 # bulk_matrices()
-
-
-
 
 def i_x_s(S, x):
 
@@ -155,8 +152,9 @@ def i_x_s(S, x):
 matrices_X = load_matrix_X()
 
 
-
 def m_zero(k,epsilon):
+
+    #   Function for expected value spread when no information is used
     
     vals=[]
     
@@ -180,28 +178,36 @@ def m_zero(k,epsilon):
 
 
 
-seed_set = []
 
+seed_set = []
 
 A = set(G_base.nodes())
 
 
 def exponential_probabilities(Ix, epsilon, n, m):
 
+    #   Function that return exponential value of Ix
+
     return np.exp(((epsilon*n)/(m*2))*Ix)
 
 
-# chose a matrix according to the size m that we want to plot
 
 
-m_values = [10,20,30,40,50,75]
+#-----------------------------------------------------------------
 
+# List that defines the number of rows of X used.
+m_values = [10,20,30,40,50,80]
+
+# List of the epsilon values
 epsilon_values = [0.01, 1]
+
 
 
 for eps in epsilon_values:
     
     m_zero(k,eps)
+
+    # Iteration over the size of the submatrix of X of size m.
 
     for h in m_values:
 
@@ -209,16 +215,20 @@ for eps in epsilon_values:
 
         c_matrix_C = copy.deepcopy(matrices_X)
 
-     
+        # Iteration over the number of influence cascades
+
         for w in range(40):
             
             sub_matrix_X = c_matrix_C[w][0:h, :]
             print("Realization " + str(w) + "size of m " + str(h))
+
+            # Times that algortihm runs for influence cascade, each value f m, and epsilon.
             
             for chi in range(5):
                 
                 seed_set = []
-                
+
+                # Iteration over the size of the seed set
     
                 for i in range(k):
     
@@ -230,6 +240,9 @@ for eps in epsilon_values:
                     iter_set = Q.difference(B)
                     list_S_minus_seed = list(iter_set)
                     table = np.zeros((len(iter_set), 3))
+
+
+                    # Iteration over the nodes that have not been selected yet
     
                     for j in range(len(iter_set)):
                         
@@ -244,12 +257,14 @@ for eps in epsilon_values:
     
                         S_union_v = copy.deepcopy(seed_set)
     
-                    # transform to probability
-    
+                    # Transform from I_x_s values to probabilities
+
                     probabilities = np.array([exponential_probabilities(
                         table[f, 1], eps, n, h) for f in range(len(iter_set))])
                     table[:, 2] = probabilities/np.sum(probabilities)
-    
+
+                    #Sampling a node with the probablities found above
+
                     seed_set.append(int(np.random.choice(
                         list(table[:, 0]), 1, p=list(table[:, 2]))[0]))
     
