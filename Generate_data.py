45dee062685366e939e8c1f3864009ecdffe53ff
carloshurtado <CAH259@pitt.edu>
2023-05-14 18:26:42 -0400

    Update the method to get one realization of live_arc_graph.

--- Generate_data.py
+++ Generate_data.py
@@ -1,3 +1,5 @@
+# Auxiliary script to generate matrices X from a networkx graph
+
 import networkx as nx
 import numpy as np
 import pandas as pd
@@ -5,145 +7,70 @@ import random
 import copy
 import pickle
 from scipy.stats import bernoulli
-# import matplotlib.pyplot as plt
-import multiprocessing
-import time
-
-# Parameters of the network
-# Parameters of College Network
 
 
-n = 2426                    # Number of nosde
-m = 250                     # Number of nodes samples to built matrix X for each influence cascade
-k = 4                       # Number of the seed set
-p_ic = 0.05                 # Probability for Independent Cascade Model
-s_bulk= 80                 # Number of Influence cascade model
-# epsilon = 1e-1
+# Settings
 
-
-# # Parameters of the network
-# # Parameters of Tweeter Network
-# # #
-# n = 3225                    # Number of nodes
-# m = 250                     # Number of nodes samples to built matrix X for each influence cascade
-# k = 8                       # Number of the seed set
-# p_ic = 0.3                # Probability for Independent Cascade Model
-# s_bulk=10                   # Number of Influence cascade model
-# # # epsilon = 1e-1
-# #
-
-
-    #
-# G_base = nx.read_edgelist("Contact-diaries-network_data_2013.csv.gz",nodetype=int, data=(("Type", str),))
-# G_base=G_base.to_undirected()
-
-
-
+m = 250  # Number of nodes samples to built matrix X for each influence cascade
+p_ic = 0.05  # Probability for Independent Cascade Model
+N=1 # Number of Influence cascade model
 
 G_base = nx.read_edgelist("soc-hamsterster_v2.edgelist.txt")
 G_base=G_base.to_undirected()
-
-
-
-# G_base = nx.read_edgelist("rt_occupy_v2.txt")
-# G_base=G_base.to_undirected()
 previous_n_labels=G_base.nodes()
 new_labes=list(range(len(previous_n_labels)))
 dictionary_mapping = dict(zip(previous_n_labels, new_labes))
 G_base = nx.relabel_nodes(G_base, dictionary_mapping)
 
 
+def generate_live_arc_graph(adj_matrix, p_ic):
+    UP = np.triu(adj_matrix, k=1)
+    boolean_mask = np.random.choice([True, False], len(np.where(UP == 1)[0]), p=[1 - p_ic, p_ic])
+    UP[np.where(UP == 1)[0][boolean_mask], np.where(UP == 1)[1][boolean_mask]] = 0
+    cascade = UP + UP.T
+    G = nx.from_numpy_matrix(cascade)
+
+    return G
 
 
-# previous_n_labels=G_base.nodes()
-# new_labes=list(range(len(previous_n_labels)))
-# dictionary_mapping = dict(zip(previous_n_labels, new_labes))
-# G_base = nx.relabel_nodes(G_base, dictionary_mapping)
-#
-
-
-# deg_dis=[G_base.degree(i) for i in range(1,1899)]
-
-
-
-def live_edges_saving(n, p, G):
-    #    Function to generate Influence cascades.
-    #   Input parameter are n: number of nodes, p: probability of the ICM, the network G.
-
-    L = len(G.edges)
-
-    tabular = np.zeros((L, 1))
-
-    Lista = bernoulli.rvs(p=p, size=L)
-    tabular = pd.DataFrame(tabular)
-    tabular[0] = G.edges()
-
-    sampled_edges = list(tabular[[i == 1 for i in Lista]][0])
-
-    # print(sampled_edges)
-    ini_nodes = set(G.nodes())
-    sG = G.edge_subgraph(sampled_edges).copy()
-    s_nodes = set(sG.nodes())
-    diff_nodes = list(ini_nodes.difference(s_nodes))
-
-    for i in diff_nodes:
-        sG.add_node(i)
-
-    # path='Live_edges\live_edges'+ str(step)+'_'+str(sample_size)+'.pkl'
-    # pickle.dump(sG, open(path, 'wb'))
-
-    return sG
-
-
-def save_graph_in_list(m, G_base, p_ic):
+def save_graph_in_list(G_base, p_ic,N):
     #   This function generates a list where each element is an influence cascade network
     #   Input as the parameters defined globally.
 
     test_keys = list(G_base.nodes())
     test_values = list(range(len(test_keys)))
     dict_to_map = {test_keys[i]: test_values[i] for i in range(len(test_keys))}
-
     H = nx.relabel_nodes(G_base, dict_to_map)
     H = H.to_undirected()
-
-    n = len(H.nodes())
-    n_e = len(H.edges())
-
-    m_live_edges = [live_edges_saving(n_e, p_ic, H) for i in range(m)]
-
-    # with open('live_edges.pkl', 'wb') as f:
-    #     pickle.dump(m_live_edges, f)
+    m_live_edges = [live_edges_saving(p_ic, H) for i in range(N)]
 
     return m_live_edges
 
 
-# save_graph_in_list(m, G_base, p_ic)
-
-
 def load_subgraphs():
-    # Function to load pickle files with the list of Influence cascade networks
+    # Function to load pickle files with the list of influence cascade networks
 
     with open('live_edges.pkl', 'rb') as f:
         m_live_edges = pickle.load(f)
 
     return m_live_edges
 
+def built_matrix_X(m, live_edges):
 
-def built_matrix_X(m, n, m_live_edges):
     # This function generates the matrix X of dimension mxn
     # Where m and n are the global parameters
 
+    indices = list(live_edges.nodes())
+    n=len(indices)
     matrix_X = np.zeros((m, n))
-    indices = list(m_live_edges[0].nodes())
 
     uniform_nodes = np.random.choice(indices, size=m, replace=True)
 
     for i in range(m):
         matrix_X[i, :] = np.array(
-            [nx.has_path(m_live_edges[i], j, uniform_nodes[i]) for j in range(n)])
+            [nx.has_path(live_edges, j, uniform_nodes[i]) for j in range(n)])
 
     return matrix_X
-
 
 def generate_random_order(G_base):
 
@@ -157,7 +84,6 @@ def generate_random_order(G_base):
 
     return order_nodes
 
-
 def load_order_nodes():
 
     with open('order_nodes.pkl', 'rb') as f:
@@ -165,30 +91,21 @@ def load_order_nodes():
 
     return order_nodes
 
-
-
-
-
-def bulk_matrices( m, n, p_ic,iter):
+def set_matrices( m, N, p_ic):
     #   This function generates and save a set of X matrices.
-    #   The number of matrices are defined by bulk_matrices parameter
+    #   The number of matrices are defined by N parameter
     #   Return a list with the matrices X and save it as pkl file
 
-
     G_float = copy.deepcopy(G_base)
-    m_live_edges = save_graph_in_list(m, G_float, p_ic)
-    output_matrix= (built_matrix_X(m, n, m_live_edges))
+    m_live_edges = save_graph_in_list(G_float, p_ic,N)
+    output_matrix= [built_matrix_X(m,m_live_edges[i]) for i in range(N)]
 
-
-    file_name='./Matricesx/matrices_X_' +str(iter) + '.pkl'
+    file_name='./matrices_X.pkl'
 
     with open(file_name, 'wb') as f:
         pickle.dump(output_matrix, f)
 
-    return output_matrix
-
-
-
+    return 0
 
 def load_matrix_X(iter):
     # Function to load pickle file with already generate X matrices
@@ -199,7 +116,6 @@ def load_matrix_X(iter):
         matrix_X = pickle.load(f)
 
     return matrix_X
-
 
 def load_matrices_X():
     # Function to load pickle file with already generate X matrices
@@ -212,50 +128,8 @@ def load_matrices_X():
     return matrix_X
 
 
-
-
-
 if __name__ == "__main__":
 
-    processes=[]
+    set_matrices(m, N, p_ic)
 
-    for t in range(s_bulk):
-
-        p=multiprocessing.Process(target=bulk_matrices, args=[ m, n, p_ic,t])
-        p.start()
-        processes.append(p)
-
-
-    for process in processes:
-
-        process.join()
-
-    # p1=multiprocessing.Process(target=bulk_matrices, args=[ m, n, p_ic,0])
-    # p2=multiprocessing.Process(target=bulk_matrices, args=[ m, n, p_ic,1])
-    #
-    # p1.start()
-    # p2.start()
-    #
-    # p1.join()
-    # p2.join()
-
-    Matrix_X=[]
-
-    for i in range(s_bulk):
-
-        Matrix_X.append(load_matrix_X(i))
-
-
-    file_name = 'Matrix_X' + '.pkl'
-
-    with open(file_name, 'wb') as f:
-
-        pickle.dump(Matrix_X,f)
-
-
-
-    with open('Matrix_X.pkl', 'rb') as f:
-        matrix_X = pickle.load(f)
-
-    # bulk_matrices(s_bulk, m, n, p_ic,1)
 
