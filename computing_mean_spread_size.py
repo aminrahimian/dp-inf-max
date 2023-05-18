8673fae8e60999b407522d4e26999ebc4d635f18
Amin Rahimian <rahimian.amin@gmail.com>
2023-05-18 15:10:14 -0400

    Rename computing_exp_spread_size.py to computing_mean_spread_size.py
    since exp can be confused for exponential mechanism

---
+++ computing_mean_spread_size.py
@@ -0,0 +1,184 @@
+# expected spread size for different  DP algorithms
+
+from dp_models import *
+
+do_computation_exp_mech=False
+do_computation_randomized=False
+do_generate_matrix_x_randomized=False
+do_computation_randomized_without_post_pr=True
+
+
+
+parameters_exp_mech={
+            'matrix_iter_val': list(range(80)),
+            'm_values' : [5,10, 20,30, 40, 60, 80],
+            'list_k' : [4,8],
+            'epsilon_values' : [0.1, 1,10],
+            'runs_alg':40,
+            'save_computation':True,
+            'number_CPU':4,
+            'output_file_name':'exp_mech.csv'}
+
+
+parameters_randomized_version={
+            'matrix_iter_val': list(range(40)),
+            'm_values' : [0,25,100],
+            'list_k' : [4,8],
+            'epsilon_values':[0.1,0.8],
+            'runs_alg':1,
+            'save_computation':True,
+            'number_CPU':4,
+            'output_file_name':'randomized_version.csv'}
+
+
+
+def load_matrices_x():
+
+    # Function to load pickle file with already generate X matrices
+    file_name = 'matrices_x.pkl'
+
+    with open(file_name, 'rb') as f:
+        matrix_X = pickle.load(f)
+
+    return matrix_X
+
+
+def dump_expected_spread_exp_mech(list_matrices_x,matrix_iter_val, m_values,list_k,
+                                  epsilon_values,runs_alg,save_computation,number_CPU
+                                  ,output_file_name):
+
+    arguments=product(matrix_iter_val, m_values,list_k, epsilon_values)
+    arguments=[list(i) for i in arguments]
+    new_args=[]
+
+    copy_arguments=copy.deepcopy(arguments)
+
+    for i in range(len(arguments)):
+        temp=arguments[i]
+        temp[0]=list_matrices_x[int(temp[0])]
+        new_args.append(temp)
+
+    new_args=new_args*runs_alg
+
+    table_parameters=np.array(copy_arguments*runs_alg)
+
+    with multiprocessing.Pool(processes=number_CPU) as pool:
+        result=pool.starmap(expect_spread_exp_mechanism, new_args)
+
+    pool.close()
+    pool.join()
+
+    if save_computation:
+
+        df=pd.DataFrame(np.concatenate((table_parameters, np.array(result).reshape(table_parameters.shape[0],1)), axis=1),
+                             columns=['arc_live','m','k','epsilon','ixs'])
+
+        df.groupby(['m', 'k', 'epsilon'], as_index=False).agg(
+            ixs_mu=('ixs', 'mean'),
+            ixs_sd=('ixs', 'std'),
+            ixs_n=('ixs', 'count')).to_csv(output_file_name, index=False)
+
+def dump_expected_spread_randomized_response(list_matrices_x,matrix_iter_val, m_values,list_k,
+                                  epsilon_values,runs_alg,save_computation,number_CPU,
+                                  output_file_name):
+
+    arguments=product(matrix_iter_val, m_values,list_k, epsilon_values)
+    arguments=[list(i) for i in arguments]
+    new_args=[]
+
+    dict_matrices_c=fill_matrix_c(list_k,epsilon_values)
+    copy_arguments=copy.deepcopy(arguments)
+
+    for i in range(len(arguments)):
+
+        temp=arguments[i]
+        print("This is " +str(temp) )
+        original_matrix=list_matrices_x[int(temp[0])]
+        temp[0]=generate_x_tilde(original_matrix, temp[3])
+        temp.append(dict_matrices_c)
+        temp.append(original_matrix)
+        new_args.append(temp)
+
+    new_args=new_args*runs_alg
+
+    table_parameters=np.array(copy_arguments*runs_alg)
+
+    with multiprocessing.Pool(processes=number_CPU) as pool:
+        result=pool.starmap(expect_spread_randomized_resp, new_args)
+
+    pool.close()
+    pool.join()
+
+    if save_computation:
+
+        df=pd.DataFrame(np.concatenate((table_parameters, np.array(result).reshape(table_parameters.shape[0],1)), axis=1),
+                             columns=['arc_live','m','k','epsilon','ixs'])
+
+        df.groupby(['m', 'k', 'epsilon'], as_index=False).agg(
+            ixs_mu=('ixs','mean'),
+            ixs_sd=('ixs', 'std'),
+            ixs_n=('ixs', 'count')).to_csv(output_file_name, index=False)
+
+
+
+def dump_expected_spread_randomized_response_wpp(list_matrices_x,matrix_iter_val, m_values,list_k,
+                                  epsilon_values,runs_alg,save_computation,number_CPU,
+                                  output_file_name):
+
+    arguments=product(matrix_iter_val, m_values,list_k, epsilon_values)
+    arguments=[list(i) for i in arguments]
+    new_args=[]
+
+    dict_matrices_c=fill_matrix_c(list_k,epsilon_values)
+    copy_arguments=copy.deepcopy(arguments)
+
+    for i in range(len(arguments)):
+
+        temp=arguments[i]
+        print("This is " +str(temp) )
+        original_matrix=list_matrices_x[int(temp[0])]
+        temp[0]=generate_x_tilde(original_matrix, temp[3])
+        temp.append(dict_matrices_c)
+        temp.append(original_matrix)
+        new_args.append(temp)
+
+    new_args=new_args*runs_alg
+
+    table_parameters=np.array(copy_arguments*runs_alg)
+
+    with multiprocessing.Pool(processes=number_CPU) as pool:
+        result=pool.starmap(expect_spread_randomized_resp_wpp, new_args)
+
+    pool.close()
+    pool.join()
+
+    if save_computation:
+
+        df=pd.DataFrame(np.concatenate((table_parameters, np.array(result).reshape(table_parameters.shape[0],1)), axis=1),
+                             columns=['arc_live','m','k','epsilon','ixs'])
+
+        df.groupby(['m', 'k', 'epsilon'], as_index=False).agg(
+            ixs_mu=('ixs','mean'),
+            ixs_sd=('ixs', 'std'),
+            ixs_n=('ixs', 'count')).to_csv(output_file_name, index=False)
+
+
+
+
+if __name__ == "__main__":
+
+    list_matrices_x = load_matrices_x()
+
+    if do_computation_exp_mech:
+
+        dump_expected_spread_exp_mech(list_matrices_x,**parameters_exp_mech)
+
+    if do_computation_randomized:
+
+        dump_expected_spread_randomized_response(list_matrices_x, **parameters_randomized_version)
+
+    if do_computation_randomized_without_post_pr:
+        parameters_randomized_version['output_file_name']='randomized_version_wpp.csv'
+        dump_expected_spread_randomized_response(list_matrices_x, **parameters_randomized_version)
+
+
