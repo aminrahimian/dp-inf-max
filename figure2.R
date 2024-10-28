acb4d259c7b4c781e3aa85ce7f861d37e6454566
YuxinLiu1997 <yul435@pitt.edu>
2024-10-28 14:48:54 -0400

    figure_2

--- figure2.R
+++ figure2.R
@@ -9,31 +9,31 @@ library(dplyr)
 library(tidyr)
 
 
-df1<-read.csv('./data_email-Eu-core/exp_mech.csv')
-df2<-read.csv('./data_email-Eu-core/randomized_version.csv')
-df3 <- read.csv('./data_email-Eu-core/greedy_alg.csv')
-greedy_vals=read.csv('./data_email-Eu-core/greedy_alg_reference.csv')
+df1<-read.csv('./data_msm_network/exp_mech.csv')
+df2<-read.csv('./data_msm_network/randomized_version.csv')
+df3 <- read.csv('./data_msm_network/greedy_alg.csv')
+greedy_vals=read.csv('./data_msm_network/greedy_alg_reference.csv')
 
 
 df1 <-df1 %>%mutate(algorithm= rep('exponential\nmechanism',dim(.)[1])) %>%  
-  mutate_at(c('m', 'k','epsilon','algorithm'), as.factor) %>% 
-  filter(k!=12)
+  mutate_at(c('m', 'k','epsilon','algorithm'), as.factor) 
+
 
 df2<- df2 %>%mutate(algorithm= rep('randomized\nresponse',dim(.)[1])) %>%  
-  mutate_at(c('m', 'k','epsilon','algorithm'), as.factor) %>% 
-  filter(k!=12)
+  mutate_at(c('m', 'k','epsilon','algorithm'), as.factor) 
+ 
 
 
 df3<- df3 %>%mutate(algorithm= rep('non-private\nalgorithm',dim(.)[1])) %>%  
   mutate_at(c('m', 'k','epsilon','algorithm'), as.factor) %>% 
-  mutate(epsilon=NA) %>% filter(k!=12)
+  mutate(epsilon=NA) 
 
 
 df<-rbind(df1,df2,df3)
 
 df<- df %>% mutate(s=paste('k = ',k, sep = ""))
-df$s<- factor(df$s, levels = c('k = 4', 'k = 8'), ordered = TRUE) 
-hline_dat = data.frame(s=c('k = 4', 'k = 8'),
+df$s<- factor(df$s, levels = c('k = 10', 'k = 20'), ordered = TRUE) 
+hline_dat = data.frame(s=c('k = 10', 'k = 20'),
                        threshold=c(greedy_vals[1,2],greedy_vals[2,2]))
 df<-df %>% mutate(m=as.numeric(as.character(df$m)))
 
@@ -42,9 +42,9 @@ g<- ggplot(df, aes(x=m, y=ixs_mu, colour=algorithm)) +
   geom_errorbar(aes(ymin=ixs_mu-(1.96/(sqrt(ixs_n)))*ixs_sd, ymax=ixs_mu+(1.96/(sqrt(ixs_n)))*ixs_sd), width=.05) +
   geom_point(aes(shape=(epsilon), size=0.5), size=2,na.rm =T)+
   geom_line(aes(group=interaction(epsilon, algorithm))) +
-  geom_hline(data=hline_dat, aes(yintercept=threshold,linetype = "greedy alg.\nupper bound"), 
+  geom_hline(data=hline_dat, aes(yintercept=threshold,linetype = "greedy baseline"), 
              colour="#A21212")+
-  scale_x_continuous(limits = c(0, 1500)) +
+  scale_x_continuous(limits = c(0, 3000)) +
   theme_light()+
   scale_color_manual(values=c("#F59E07","#1F618D", "#C0392B"))+
   labs(x = "number of influece samples", y="expected spread  size")+
@@ -62,7 +62,7 @@ g
 
 
 
-saving_name="email-eu-core-k-48-exp-rr.pdf"
+saving_name="HIV-network-k-1020-exp-rr.pdf"
 
 
 ggsave(saving_name, plot = g, width = 6.5, height = 4, units = "in", dpi = 300)
